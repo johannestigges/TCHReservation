08b16b3a106e2b7e6adfecfacf1d429ed27ccc49
johannes <johannes@Fabian-PC.fritz.box>
2018-07-30 22:53:24 +0200

    remove unused import

--- src/main/java/de/tigges/tchreservation/converter/LocalTimeJsonDeserializer.java
+++ src/main/java/de/tigges/tchreservation/converter/LocalTimeJsonDeserializer.java
@@ -10,7 +10,6 @@ import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 
