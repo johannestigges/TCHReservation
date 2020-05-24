99d1b4cc6fc2e0da1650a20b18181c91df701508
johannestigges <johannes.tigges@freenet.de>
2020-05-24 18:34:11 +0200

    read Reservation System Properties from application.yaml

--- src/main/java/de/tigges/tchreservation/reservation/ReservationSystemConfigRepository.java
+++ src/main/java/de/tigges/tchreservation/reservation/ReservationSystemConfigRepository.java
@@ -1,5 +1,6 @@
 package de.tigges.tchreservation.reservation;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import de.tigges.tchreservation.exception.NotFoundException;
@@ -9,17 +10,11 @@ import de.tigges.tchreservation.reservation.model.ReservationSystemConfig;
 @Component
 public class ReservationSystemConfigRepository {
 
-	private static final ReservationSystemConfig system1 = new ReservationSystemConfig(1, "Außenplätze", 6, 30, 8, 22);
-	private static final ReservationSystemConfig system2 = new ReservationSystemConfig(2, "Hallenplätze", 2, 60, 8, 22);
+	@Autowired
+	ReservationSystemConfiguration properties;
 
 	public ReservationSystemConfig get(long id) {
-		switch ((int) id) {
-		case 1:
-			return system1;
-		case 2:
-			return system2;
-		default:
-			throw new NotFoundException(EntityType.SYSTEM_CONFIGURATION, id);
-		}
+		return properties.getConfig().stream().filter(s -> id == s.getId()).findFirst()
+				.orElseThrow(() -> new NotFoundException(EntityType.SYSTEM_CONFIGURATION, id));
 	}
 }
