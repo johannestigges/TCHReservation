3ca1333f924f14abe63409922243551782d29b39
johannestigges <johannes.tigges@freenet.de>
2020-05-26 07:17:40 +0200

    remove javax.validation

--- src/main/java/de/tigges/tchreservation/reservation/model/ReservationSystemConfig.java
+++ src/main/java/de/tigges/tchreservation/reservation/model/ReservationSystemConfig.java
@@ -1,8 +1,5 @@
 package de.tigges.tchreservation.reservation.model;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,17 +13,11 @@ public class ReservationSystemConfig {
 
 	private String name;
 
-	@Min(1)
 	private int courts;
 
-	@Min(15)
 	private int durationUnitInMinutes;
 
-	@Min(0)
-	@Max(23)
 	private int openingHour;
 
-	@Min(1)
-	@Max(24)
 	private int closingHour;
 }
