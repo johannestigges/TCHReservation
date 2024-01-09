fb639fbd8861aa964d4015f83a9db83ed1b0a16c
johannestigges <johannes.tigges@freenet.de>
2024-01-09 21:25:32 +0100

    find all user order by name

--- src/main/java/de/tigges/tchreservation/user/jpa/UserRepository.java
+++ src/main/java/de/tigges/tchreservation/user/jpa/UserRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
 	Optional<UserEntity> findByNameOrEmail(String userName, String email);
+	Iterable<UserEntity> findAllByOrderByName();
 }
